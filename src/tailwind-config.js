9d628c6e4ff463d7ce88f806f89e2dcf0d17bda3
wendyhamel <whamel@it4web.nl>
2022-11-04 15:16:50 +0100

    js for switching slides.

---
+++ src/tailwind-config.js
@@ -0,0 +1,13 @@
+tailwind.config = {
+    theme: {
+        fontFamily: {
+            'sans': ['Inter', 'sans-serif'],
+        },
+        extend: {
+            colors: {
+                'gray-blue': 'hsl(240, 18%, 77%)',
+                'dark-blue': 'hsl(240, 38%, 20%)',
+            }
+        }
+    }
+}
---
+++ src/testimonials.js
@@ -0,0 +1,49 @@
+window.data = function() {
+    return {
+        testimonials: [
+            {
+                active: true,
+                image: "./images/image-tanya.jpg",
+                message: "I’ve been interested in coding for a while but never taken the jump, until now. \n" +
+                    "  I couldn’t recommend this course enough. I’m now in the job of my dreams and so \n" +
+                    "  excited about the future.",
+                name: "Tanya Sinclair",
+                job: "X Engineer",
+            },
+            {
+                active: false,
+                image: "./images/image-john.jpg",
+                message: "If you want to lay the best foundation possible I’d recommend taking this course. \n" +
+                    "  The depth the instructors go into is incredible. I now feel so confident about \n" +
+                    "  starting up as a professional developer.",
+                name: "John Tarkpor",
+                job: "Junior Front-end Developer",
+            },
+        ],
+
+        activeSlide: [],
+
+        get showSlide() {
+            this.activeSlide = this.testimonials.filter(slide => slide.active === true);
+            return this.activeSlide[0];
+        },
+
+        prevSlide() {
+            this.activeSlide[0].active = false;
+            if (this.testimonials.indexOf(this.activeSlide[0]) === 0) {
+                this.testimonials[this.testimonials.length - 1].active = true;
+            } else {
+                this.testimonials[this.testimonials.indexOf(this.activeSlide[0]) - 1].active = true;
+            }
+        },
+
+        nextSlide() {
+            this.activeSlide[0].active = false;
+            if (this.testimonials.indexOf(this.activeSlide[0]) === (this.testimonials.length - 1)) {
+                this.testimonials[0].active = true;
+            } else {
+                this.testimonials[this.testimonials.indexOf(this.activeSlide[0]) + 1].active = true;
+            }
+        }
+    }
+}
